7718faf97b7f1076b05cd195a01b6688fd2daa36
Awwal123 <awwalsalakomuhammed@gmail.com>
2025-03-13 00:34:40 +0100

    thems

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,6 +5,23 @@ import 'vuetify/styles'
 // Vuetify
 import { createVuetify } from 'vuetify'
 
-export default createVuetify(
-  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+const myCustomTheme = {
+  dark: false, // Set to `true` for dark mode
+  colors: {
+    primary: "#9652ff",
+    success: "#3cd1c2",
+    info: "#ffaa2c",
+    error: "#f83e70",
+    myCustomColor: '#9C27B0' 
+  }
+}
+
+export default createVuetify({
+  theme: {
+    defaultTheme: 'customTheme', // Set this as the default theme
+    themes: {
+      customTheme: myCustomTheme,
+    }
+  }
+}
 )
