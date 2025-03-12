cbb2b5a8a2eaf9a1317729e894dc4cb4d7af4a0e
Awwal123 <awwalsalakomuhammed@gmail.com>
2025-03-12 23:55:50 +0100

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app>
+  <v-app class="bg-grey-lighten-3">
     <Navbar />
     <v-main>
       <router-view />
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,14 @@
 <template>
-  <nav>Nav</nav>
+  <v-toolbar>
+    <v-toolbar-title class="text-uppercase text-grey">
+      <span class="font-weight-light">Todo</span>
+      <span>Ninja</span>
+    </v-toolbar-title>
+    <v-btn flat color="grey">
+      <span>Sign Out</span>
+      <v-icon right class="ml-2">mdi-exit-to-app</v-icon>
+    </v-btn>
+  </v-toolbar>
 </template>
 <script>
 export default {
