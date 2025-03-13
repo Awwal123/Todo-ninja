6c90f9af98b22cef21b976b3f1420ca7652781de
Awwal123 <awwalsalakomuhammed@gmail.com>
2025-03-14 00:04:20 +0100

    lesson 13, grid-system

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,7 +2,20 @@
   <div class="dashboard">
     <h1 class="text-grey text-h6">Dashboard</h1>
     <v-container class="my-5">
-     content
+      <v-row>
+        <v-col cols="12" md="6">
+          <v-btn variant="outline" block class="primary">1</v-btn>
+        </v-col>
+        <v-col cols="4" md="2">
+          <v-btn variant="outline" block class="primary">2</v-btn>
+        </v-col>
+        <v-col cols="4" md="2">
+          <v-btn variant="outline" block class="primary">3</v-btn>
+        </v-col>
+        <v-col cols="4" md="2">
+          <v-btn variant="outline" block class="primary">4</v-btn>
+        </v-col>
+      </v-row>
     </v-container>
   </div>
 </template>
