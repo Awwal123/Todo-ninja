8fb3bf836a2a45e044af89ffebb8a5b588bbcf76
Awwal123 <awwalsalakomuhammed@gmail.com>
2025-03-14 00:24:33 +0100

    lesson 14 grid cont..

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,20 +2,26 @@
   <div class="dashboard">
     <h1 class="text-grey text-h6">Dashboard</h1>
     <v-container class="my-5">
-      <v-row>
-        <v-col cols="12" md="6">
-          <v-btn variant="outline" block class="primary">1</v-btn>
-        </v-col>
-        <v-col cols="4" md="2">
-          <v-btn variant="outline" block class="primary">2</v-btn>
-        </v-col>
-        <v-col cols="4" md="2">
-          <v-btn variant="outline" block class="primary">3</v-btn>
-        </v-col>
-        <v-col cols="4" md="2">
-          <v-btn variant="outline" block class="primary">4</v-btn>
-        </v-col>
-      </v-row>
+      <v-card flat class="pa-3">
+        <v-row>
+          <v-col cols="12" md="6">
+            <div class="caption text-grey">Project Title</div>
+            <div>Create a new Project</div>
+          </v-col>
+          <v-col cols="12" sm="4" md="2">
+            <div class="caption text-grey">Person</div>
+            <div>The Net Ninja</div>
+          </v-col>
+          <v-col cols="12" sm="4" md="2">
+            <div class="caption text-grey">Due by</div>
+            <div>1st Jan 2025</div>
+          </v-col>
+          <v-col cols="12" sm="4" md="2">
+            <div class="caption text-grey">Status</div>
+            <div>Ongoing</div>
+          </v-col>
+        </v-row>
+      </v-card>
     </v-container>
   </div>
 </template>
