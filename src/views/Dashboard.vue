a0dc4bc492e73908bb9c379854eac68f1eede13f
Awwal123 <awwalsalakomuhammed@gmail.com>
2025-03-14 12:00:37 +0100

    lesson 18 (tooltip)

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,16 +2,41 @@
   <div class="dashboard">
     <h1 class="text-grey text-h6">Dashboard</h1>
     <v-container class="my-5">
-      <v-row class="mb-3  ml-2 d-flex gap-2">
-         <v-btn flat size="small" color="grey lighten-4" class="mr-2" @click="sortBy('title')">
-          <v-icon class="mr-3">mdi-folder</v-icon>
-          <span class="caption text-lowercase">By project name</span>
-         </v-btn>
-         <v-btn flat size="small" color="grey lighten-4" @click="sortBy('person')">
-          <v-icon class="mr-3">mdi-account</v-icon>
-          <span class="caption text-lowercase">By person</span>
-         </v-btn>
+      <v-row class="mb-3 ml-2 d-flex gap-2">
+        <v-tooltip location="top">
+          <template v-slot:activator="{ props }">
+            <v-btn
+              v-bind="props"
+              flat
+              size="small"
+              color="grey lighten-1"
+              class="mr-2"
+              @click="sortBy('title')"
+            >
+              <v-icon class="mr-3">mdi-folder</v-icon>
+              <span class="caption text-lowercase">By project name</span>
+            </v-btn>
+          </template>
+          <span>Sort projects by title</span>
+        </v-tooltip>
+
+        <v-tooltip location="top">
+          <template v-slot:activator="{ props }">
+            <v-btn
+              v-bind="props"
+              flat
+              size="small"
+              color="grey lighten-1"
+              @click="sortBy('person')"
+            >
+              <v-icon class="mr-3">mdi-account</v-icon>
+              <span class="caption text-lowercase">By person</span>
+            </v-btn>
+          </template>
+          <span>Sort projects by person name</span>
+        </v-tooltip>
       </v-row>
+
       <v-card
         v-for="(project, index) in projects"
         :key="index"
@@ -33,12 +58,12 @@
           </v-col>
           <v-col cols="12" sm="4" md="2" class="text-right">
             <!-- <div class="right"> -->
-              <v-chip
-                size="small"
-                :class="`my-2 caption text-white ${project.status}`"
-              >
-                {{ project.status }}
-              </v-chip>
+            <v-chip
+              size="small"
+              :class="`my-2 caption text-white ${project.status}`"
+            >
+              {{ project.status }}
+            </v-chip>
             <!-- </div> -->
           </v-col>
         </v-row>
