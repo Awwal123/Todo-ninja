d16ae68acab16f6c658a32a4e2ffaaac77239f48
Awwal123 <awwalsalakomuhammed@gmail.com>
2025-03-19 10:31:16 +0100

    lesson 22 (menu)

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,6 +10,22 @@
         <span>Ninja</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
+
+      <v-menu>
+        <template v-slot:activator="{props}">
+        <v-btn v-bind="props" flat color="grey">
+          <v-icon left class="mx-2">mdi-menu-down</v-icon>
+          <span>Menu</span>
+        </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+            <v-list-item-title>{{ link.text }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
       <v-btn flat color="grey">
         <span>Sign Out</span>
         <v-icon right class="ml-2">mdi-exit-to-app</v-icon>
@@ -49,7 +65,8 @@ export default {
         {icon: "mdi-home", text:  "Dashboard", route: "/" },
         {icon: "mdi-folder", text:  "My Project", route: "/projects" },
         {icon: "mdi-account", text:  "Team", route: "/team" }
-      ]
+      ],
+      menuItems: ["Profile", "Settings", "Logout"],
     }
   }
 }
