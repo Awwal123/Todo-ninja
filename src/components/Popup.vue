b34f06537b8190da36b168689003412f6c17f21d
Awwal123 <awwalsalakomuhammed@gmail.com>
2025-03-20 03:02:16 +0100

    clean up

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -29,7 +29,7 @@
   export default {
     data() {
       return {
-        dialog: false, // This was missing
+        dialog: false, 
         projectTitle: "",
         projectDescription: "",
       };
